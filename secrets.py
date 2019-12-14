9e054d35e578d9de2ae4100ebd3d241982a77b15
Phil Moyer <hukuzatuna@gmail.com>
2019-12-14 13:23:04 -0800

    Initial commit of example file.

---
+++ secrets.py
@@ -0,0 +1,13 @@
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+# which would be not great. So, instead, keep it all in this one file and
+# keep it a secret.
+
+secrets = {
+    'ssid' : 'YOUR_WIFI_SSID',            # Keep the two '' quotes around the name
+    'password' : 'YOUR_WIFI_PASSWORD',    # Keep the two '' quotes around password
+    'timezone' : "America/Los_Angeles",   # http://worldtimeapi.org/timezones
+    'aio_username' : 'YOUR_AIO_USERNAME', # Keep the quotes again
+    'aio_key' : 'YOUR_AIO_KEY',           # Keep the quotes again
+    }
+
